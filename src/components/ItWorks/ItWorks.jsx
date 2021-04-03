c3f816ebf874eba127d24690efd8d58a0b08e201
mavro <mrojasb2000@gmail.com>
2021-04-03 17:16:39 -0300

    Added basic component

--- src/components/ItWorks/ItWorks.jsx
+++ src/components/ItWorks/ItWorks.jsx
@@ -1,8 +1,14 @@
 import React from "react"
 import FirstComp from "../FirstComp/FirstComp";
+
+const Welcome = (props) => {
+    return <h3>Hello, {props.name}</h3>;
+}
+
 const ItWorks = () => {
     return (
         <>
+        <Welcome name="Mavro" />
         <h1>It works!</h1>
         <FirstComp />
         </>

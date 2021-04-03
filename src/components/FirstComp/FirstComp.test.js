31d6596f926f7cc0519597ff899cfaac5ac748ab
mavro <mrojasb2000@gmail.com>
2021-04-03 16:28:54 -0300

    Added subtitle unit test components

--- src/components/FirstComp/FirstComp.test.js
+++ src/components/FirstComp/FirstComp.test.js
@@ -1,10 +1,15 @@
 import React from "react"
-import { render } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import FirstComp from "./FirstComp"
 
 describe("<FirstComp/>", () => {
   
     it("should render component", () => {
       render(<FirstComp/>)
+    })
+
+    it("should render subtitle in component", () => {
+        render(<FirstComp/>)
+        expect(screen.getByText("This is first component II")).toBeDefined()
     })
 })
